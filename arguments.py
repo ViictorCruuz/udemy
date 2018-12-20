0a59610ee4d80ff505d571f89a69ff73110fb40d
Victor Cruz - Local <victor.cruz@geru.com.br>
2018-12-20 18:16:42 -0200

    be continue

--- arguments.py
+++ arguments.py
@@ -6,8 +6,8 @@ def dados_pessoais(nome, sobrenome, idade, sexo):
     print("Nome: {}\nSobrenome: {}\nIdade: {}\nSexo: {}"
             .format(nome, sobrenome, idade, sexo))
 
-# dados_pessoais("Victor", "Cruz", 19, True)  # > Passados argumentos de forma posicional
-dados_pessoais(sobrenome="Cruz",
-                sexo=True,
-                nome="Victor",
-                idade=19) # > Passados argumentos de forma nomeado
+dados_pessoais("Victor", "Cruz", 18, True)  # > Passados argumentos de forma posicional
+#   dados_pessoais(sobrenome="Cruz",
+#                 sexo=True,
+#                 nome="Victor",
+#                 idade=19) # > Passados argumentos de forma nomeado
--- exunpacking.py
+++ exunpacking.py
@@ -3,16 +3,15 @@
 # EXEMPLO DE DESEMPACOTAMENTO
 
 lista = [11, 10, 12]
-tupla = 11, 10, 12
+tupla = (11, 10, 12)
+
 
 def func(a, b, c):
     print(a)
     print(b)
     print(c)
 
-# lista.sort()
-# func(*lista)
-
 l = [*tupla]
+# l = list(tupla)
 l.sort()
 func(*l)
