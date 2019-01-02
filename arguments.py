ed69ca9bc87092166648bbba233840665418fc4a
VictorCruz <victor.victor2012@hotmail.com>
2019-01-02 21:11:58 -0200

    retomando

--- arguments.py
+++ arguments.py
@@ -2,12 +2,21 @@
 
 ## ARGUMENTOS POSICIONAIS E ARGUMENTOS NOMEADOS
 
-def dados_pessoais(nome, sobrenome, idade, sexo):
-    print("Nome: {}\nSobrenome: {}\nIdade: {}\nSexo: {}"
-            .format(nome, sobrenome, idade, sexo))
+#def dados_pessoais(nome, sobrenome, idade, sexo):
+#    print("Nome: {}\nSobrenome: {}\nIdade: {}\nSexo: {}"
+#            .format(nome, sobrenome, idade, sexo))
 
-dados_pessoais("Victor", "Cruz", 18, True)  # > Passados argumentos de forma posicional
+#dados_pessoais("Victor", "Cruz", 18, True)  # > Passados argumentos de forma posicional
 #   dados_pessoais(sobrenome="Cruz",
 #                 sexo=True,
 #                 nome="Victor",
 #                 idade=19) # > Passados argumentos de forma nomeado
+
+def ficha_de_cadastro(nome, sobrenome, idade, sexo):
+    print("Nome: {}\nSobrenome: {}\nIdade: {}\nSexo: {}"
+            .format(nome, sobrenome, idade, sexo))
+
+ficha_de_cadastro(sobrenome="Cruz",
+                  sexo=True,
+                  nome="Victor",
+                  idade=20)
--- unpacking.py
+++ unpacking.py
@@ -2,15 +2,26 @@
 
 # DESEMPACOTAMENTO I
 
-def pessoa(nome, sobrenome, idade):
+#def pessoa(nome, sobrenome, idade):
+#    print(nome)
+#    print(sobrenome)
+#    print(idade)
+
+#lista = ["Brasil", "exCript", 20]
+# pessoa(tupla[0], tupla[1], tupla[2])
+
+#pessoa(*lista)
+
+# d = {"nome":"exCript", "sobrenome":"Brasil", "idade":20 }
+# pessoa(**d)
+
+
+
+def ficha(nome, sobrenome, idade):
     print(nome)
     print(sobrenome)
     print(idade)
 
-lista = ["Brasil", "exCript", 20]
-# pessoa(tupla[0], tupla[1], tupla[2])
+lista = ["Victor", "Cruz", 20]
 
-pessoa(*lista)
-
-# d = {"nome":"exCript", "sobrenome":"Brasil", "idade":20 }
-# pessoa(**d)
+ficha(*lista)
