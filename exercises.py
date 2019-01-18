9f09e7c48505e604bf5baa8b685ac405aea8df0f
Victor Cruz - Local <victor.cruz@geru.com.br>
2019-01-18 16:04:11 -0200

    new exercise

--- exercises.py
+++ exercises.py
@@ -129,12 +129,12 @@ Invoque a função func(), passando como argumento os valores contidos em lista,
 utilizar o conceito de desempacotamento:
 """
 
-def func(a, b, c, d):
-    print(a, b, c, d)
-
-lista = 1,2,3,4
-
-func(*lista)
+# def func(a, b, c, d):
+#     print(a, b, c, d)
+#
+# lista = 1,2,3,4
+#
+# func(*lista)
 
 # RESOLVIDO :)
 
